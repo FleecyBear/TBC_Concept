2c18f8b6ce9c0bee16c8181d018b29b6c2aae4d2
FleecyBear <nikusha.kk1990@gmail.com>
2024-07-28 09:35:35 +0400

    Fixed phone
    added box to display end-drop menu better for phone;
    added width errors for phone;
    fixed images to be visible from GITHUB site

--- toggle.js
+++ toggle.js
@@ -1,19 +1,18 @@
 
 const toggleButton = document.querySelector('.hidden-button-2');
 const unexpandElement = document.querySelector('.unexpand');
-
+const unhideButton = document.querySelector('.unhide-button');
 
 function toggleUnexpand() {
     if (unexpandElement.style.display === 'none' || !unexpandElement.style.display) {
         unexpandElement.style.display = 'block';
-        toggleButton.backgroundImage
-
+        unhideButton.style.display = 'block';
        
         document.body.style.height = 'auto'; 
         window.scrollTo(0, document.body.scrollHeight); 
     } else {
         unexpandElement.style.display = 'none';
-    
+        unhideButton.style.display = 'block';
         
         
         document.body.style.height = 'auto'; 
@@ -30,7 +29,7 @@ function adjustForScreenSize() {
     
     if (screenWidth > 1000) {
         unexpandElement.style.display = 'block';
-       
+        unhideButton.style.display = 'none';
     } else {
        
     
